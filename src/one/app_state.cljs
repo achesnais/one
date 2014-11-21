3d1aaba39ecb559fc13218ef35597efc19499799
Brenton Ashworth <brenton@cognitect.com>
2014-11-21 16:08:36 -0500

    Document one.app-state

--- src/one/app_state.cljs
+++ src/one/app_state.cljs
@@ -1,9 +1,17 @@
 (ns one.app-state
-  "Functions for getting data out of and updating app state."
+  "Instead of spreading knowledge of the structure of app state all
+  over the application, confine that knowledge to this one
+  namespace. Provides semantic functions for accessing and updating
+  app state."
   (:require [om.core :as om]))
 
-(defn current-view [app-state]
+(defn current-view
+  "Given the app-state cursor or value, return the current view."
+  [app-state]
   (:view app-state))
 
-(defn set-view! [app-state view]
+(defn set-view!
+  "Given the app-state cursor and a view, make the provided view the
+  current view."
+  [app-state view]
   (om/update! app-state :view view))
